5d6401764cab40a3635edac5345c3ea5f593275d
coa <coa.andu@gmail.com>
2013-07-29 19:38:13 +0900

    initail

---
+++ src/comaDisq.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxNI2.h"
+#include "ofxNiTE2.h"
+#include "OpenNI.h"
+
+class comaDisco : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+        void exit();
+
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+        ofxNI2::Device device;
+        ofxNI2::IrStream ir;
+        ofxNiTE2::UserTracker tracker;
+        ofImage depth_image;
+        ofEasyCam cam;
+};
